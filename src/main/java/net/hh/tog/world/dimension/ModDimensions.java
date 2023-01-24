8a38c9f6059a875c9bb856dc35fc79f8c49fda6b
Jofresh77 <eliot.hoff7@gmail.com>
2023-01-24 17:46:58 +0100

    Dimension registry test (fail)

---
+++ src/main/java/net/hh/tog/world/dimension/ModDimensions.java
@@ -0,0 +1,17 @@
+package net.hh.tog.world.dimension;
+
+import net.fabricmc.fabric.mixin.registry.sync.RegistriesAccessor;
+import net.hh.tog.TogMod;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+
+public class ModDimensions {
+    /*public static final RegistryKey<World> TOGDIM_DIMENSION_KEY = RegistryKey.of();
+
+    private static World registerWorld(String name, World world) {
+        return Registry.register(Registries.ITEM, new Identifier(TogMod.MOD_ID, name), world);
+    }*/
+}
